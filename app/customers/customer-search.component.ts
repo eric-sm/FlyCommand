395614bf16546e682016869822aa86acbf3f1467
Eric Small <eric@jericsmall.com>
2016-06-23 23:09:19 -0400

    Route search results to the customer detail page

--- app/customers/customer-search.component.ts
+++ app/customers/customer-search.component.ts
@@ -1,13 +1,16 @@
 import { Component } from 'angular2/core';
 import { Subscription } from 'rxjs/Subscription';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 import { CustomerService } from './customer.service';
 import { ICustomer } from './customer';
 import { PhoneNumberFormatPipe } from '../shared/phone-number-format.pipe';
+
 
 @Component({
     templateUrl: 'app/customers/customer-search.component.html',
     styleUrls: ['app/customers/customer-search.component.css'],
     pipes: [PhoneNumberFormatPipe],
+    directives: [ROUTER_DIRECTIVES],
     bindings: [CustomerService]
 })
 export class CustomerSearchComponent {
