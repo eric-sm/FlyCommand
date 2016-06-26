4566eb46d2ac07f003591e01b2a163a1e0fdf937
Eric Small <eric@jericsmall.com>
2016-06-25 22:55:07 -0400

    Code cleanup: have private function start with _

--- app/providers/custom-http.http.ts
+++ app/providers/custom-http.http.ts
@@ -2,6 +2,7 @@ import {Http, Request, Headers, Response, RequestOptionsArgs, RequestOptions} fr
 import { Injectable, Inject, forwardRef} from 'angular2/core';
 import { Observable } from 'rxjs/Observable';
 
+
 @Injectable()
 export class CustomHttp {
     token: string = '0c514dd7b7e8fa4d62283491574d359ea730cdaf';
@@ -20,7 +21,7 @@ export class CustomHttp {
         this.tokenOwner = '';
     }
 
-    private addAuthorizationHeader(options?: RequestOptionsArgs): RequestOptionsArgs {
+    private _addAuthorizationHeader(options?: RequestOptionsArgs): RequestOptionsArgs {
         if (options && options.headers) {
              options.headers.append('Authorization', 'Token ' + this.token);
              options.headers.append('Identity', 'Email ' + this.tokenOwner);
@@ -41,7 +42,6 @@ export class CustomHttp {
         }
     }
 
-
     // Wrappings for each of the methods in the base Http class definition
     // with the authorization headers added
     public request(url: string | Request, options?: RequestOptionsArgs) {
@@ -49,26 +49,26 @@ export class CustomHttp {
     }
 
     public get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.get(url, this.addAuthorizationHeader(options));
+        return this.http.get(url, this._addAuthorizationHeader(options))
     }
 
     public post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {  
-        return this.http.post(url, body, this.addAuthorizationHeader(options));
+        return this.http.post(url, body, this._addAuthorizationHeader(options));
     }
 
     public put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.put(url, body, this.addAuthorizationHeader(options));
+        return this.http.put(url, body, this._addAuthorizationHeader(options));
     }
 
     public patch(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {  
-        return this.http.patch(url, body, this.addAuthorizationHeader(options));
+        return this.http.patch(url, body, this._addAuthorizationHeader(options));
     }
 
     public delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.delete(url, this.addAuthorizationHeader(options));
+        return this.http.delete(url, this._addAuthorizationHeader(options));
     }
     
     public head(url: string, options?: RequestOptionsArgs): Observable<Response>{
-        return this.http.head(url, this.addAuthorizationHeader(options));
+        return this.http.head(url, this._addAuthorizationHeader(options));
     }
 }
