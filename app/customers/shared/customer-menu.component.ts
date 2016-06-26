b92dfb61fbce3c7a3b8bf9eaa2f21c81199944cd
Eric Small <eric@jericsmall.com>
2016-06-26 06:27:01 -0400

    Toggling for order filter on side menu

--- app/customers/shared/customer-menu.component.ts
+++ app/customers/shared/customer-menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, Output, EventEmitter } from 'angular2/core';
+import { Component, Input, Output, EventEmitter, ViewChild,
+     ElementRef, AfterViewChecked, Renderer } from 'angular2/core';
 
 @Component({
     selector: 'fcc-customer-menu',
     templateUrl : 'app/customers/shared/customer-menu.component.html',
     styleUrls: ['app/customers/shared/customer-menu.component.css']
 })
-export class CustomerMenuComponent {
+export class CustomerMenuComponent implements AfterViewChecked {
+    @ViewChild('inputNumberFilter') inputNumberFilter: ElementRef;
     @Input() customerId: number;
     @Input() orderId: number;
     @Output() notifyShowCancelled: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -14,18 +16,35 @@ export class CustomerMenuComponent {
     showOrderFilter: boolean = false;
     orderNumberFilter: string;
 
-    constructor() {}
+    constructor(private _renderer: Renderer) {}
 
     toggleCancelled(): void {
         this.showCancelled = !this.showCancelled;
         this.notifyShowCancelled.emit(this.showCancelled);
     }
 
-    toggleOrderFilter() {
-        this.showOrderFilter = !this.showOrderFilter;
+    turnOnOrderFilter() {
+        this.showOrderFilter = true;
+    }
+
+    turnOffOrderFilter() {
+        this.showOrderFilter = false;
     }
 
     sendOrderFilter() {
         this.notifyOrderFilter.emit(this.orderNumberFilter);
+    }
+
+    clearOrderFilter() {
+        this.orderNumberFilter = '';
+        this.turnOffOrderFilter();
+        this.sendOrderFilter();
+    }
+
+    ngAfterViewChecked() {
+        if (this.inputNumberFilter && this.inputNumberFilter.nativeElement
+                && this.showOrderFilter) {
+            this._renderer.invokeElementMethod(this.inputNumberFilter.nativeElement, 'focus', []);
+        }
     }
 }
