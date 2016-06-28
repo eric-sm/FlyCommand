b2ed91ac32026f8be79d9a1edd41bfb7efb26b42
Eric Small <eric@jericsmall.com>
2016-06-28 03:02:49 -0400

    CustomerProfileSidebarComponent no longer used here

--- app/orders/order.component.ts
+++ app/orders/order.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit, Input } from 'angular2/core';
 import { RouteParams } from 'angular2/router';
 import { IOrder } from './order';
 import { OrderService } from './order.service';
-import { CustomerProfileSidebarComponent } from '../customers/sidebars/customer-profile-sidebar.component';
 
 @Component({
     selector: 'fcc-customer-order',
     templateUrl : 'app/orders/order.component.html',
-    styleUrls: ['app/orders/order.component.css'],
-    directives: [CustomerProfileSidebarComponent]
+    styleUrls: ['app/orders/order.component.css']
 })
 export class OrderComponent implements OnInit {
     @Input() customerId: number;
