53e4b11f39c6523a92aa3eb9aef3fca68951e1af
Eric Small <eric@jericsmall.com>
2016-07-31 23:23:44 -0400

    Remove the Receipt submenu (will be part of services)

--- app/orders/order.component.ts
+++ app/orders/order.component.ts
@@ -4,14 +4,13 @@ import { IOrder } from './order';
 import { OrderServicesComponent } from './parts/order-services.component';
 import { OrderEventsComponent } from './parts/order-events.component';
 import { OrderPreferencesComponent } from './parts/order-preferences.component';
-import { OrderReceiptComponent } from './parts/order-receipt.component';
 import { OrderFeedbackComponent } from './parts/order-feedback.component';
 
 @Component({
     selector: 'fcc-customer-order',
     templateUrl : 'app/orders/order.component.html',
     styleUrls: ['app/orders/order.component.css'], 
-    directives: [OrderServicesComponent, OrderEventsComponent, OrderPreferencesComponent, OrderReceiptComponent, OrderFeedbackComponent]
+    directives: [OrderServicesComponent, OrderEventsComponent, OrderPreferencesComponent, OrderFeedbackComponent]
 })
 export class OrderComponent {
     @Input() customerId: number;
